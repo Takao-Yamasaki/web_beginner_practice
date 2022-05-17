8b212eb3bd557e7a5511b7aec8581ecaff0d05cc
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-05-17 16:21:29 +0900

    GETで送る

--- webrick.rb
+++ webrick.rb
@@ -7,4 +7,25 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+# GETを受け取る
+server.mount_proc("/form_get") do |req, res|
+  q = req.query
+  body = <<~"HTML"
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>GET</title>
+      </head>
+      <body>
+        <p>クエリパラメータは#{q}です。</p>
+        <p>こんにちは#{q['user_name']}さん。あなたの年齢は#{q['user_age']}ですね。</p>
+      </body>
+    </html>
+  HTML
+
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 server.start
