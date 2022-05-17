2a4f36d53f2fee38a46a0f5d2dc32fd26463aab2
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-05-17 20:14:17 +0900

    POSTで送る

--- webrick.rb
+++ webrick.rb
@@ -1,13 +1,35 @@
  # webrick.rb
 require 'webrick'
 
+# POSTで送る
+class ServletAction < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(req, res)
+    q = req.query
+    res.body = <<~"HTML"
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>POST</title>
+        </head>
+        <body>
+          <p>フォームデータは#{q}です。</p>
+          <p>こんにちは#{q['user_name']}さん。あなたの年齢は#{q['user_age']}ですね。</p>
+        </body>
+      </html>
+    HTML
+    res.status = 200
+    res['Content-Type'] = 'text/html'
+  end
+end
+
+# HTTPServerを定義
 server = WEBrick::HTTPServer.new({ 
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
   :Port => 8000
 })
 
-# GETを受け取る
+# GETで送る
 server.mount_proc("/form_get") do |req, res|
   q = req.query
   body = <<~"HTML"
@@ -27,5 +49,8 @@ server.mount_proc("/form_get") do |req, res|
   res['Content-Type'] = 'text/html'
   res.body = body
 end
+
+# POSTで送る
+server.mount("/form_post", ServletAction)
 
 server.start
